57fc1a2716e5cdd75a963c2728fb3a026ca8d28d
Jung-Ah-C <wjddk718@gmail.com>
2022-11-18 17:19:04 +0900

    Add files to type html codes

---
+++ components/Button.tsx
@@ -0,0 +1,8 @@
+
+const Button: React.FC = () => {
+    return (
+        <button>Add</button>
+    );
+}
+
+export default Button;
---
+++ features/Clock/components/Clock.tsx
@@ -0,0 +1,10 @@
+
+const Clock: React.FC = () => {
+    return (
+        <div className="Clock-div">
+            <h2>2022. 11. 18. 17:00:00</h2>
+        </div>
+    );
+}
+
+export default Clock;
