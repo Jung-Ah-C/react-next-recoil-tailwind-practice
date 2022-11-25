8669b47fe16d8604ca03c94dc89e86986c01cb05
JungAh Choi (최정아) <65520261+jamie-jungah@users.noreply.github.com>
2022-11-25 15:45:10 +0900

    Revert "Add TailwindCSS code (#10)" (#11)
    This reverts commit d81bfdf8ec74161ebc06ade8dcf2b9e51f12b30e.

--- pages/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default } from "@/features/Todo/pages/TodoPage";
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{tsx,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
